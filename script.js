41767cb680502aabfa274b774f0ed888556f8f8e
JITENDRA KUMAR <jitendrakumar77800@gmail.com>
2025-02-05 19:29:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	 let charCount = {};
+
+    // Count occurrences of each character
+    for (let char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    // Find the first non-repeating character
+    for (let char of str) {
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+
+
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
